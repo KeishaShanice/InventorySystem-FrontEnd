ec115c94b62548e89f0c8dad90e59175a0d5312b
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2022-10-06 11:42:28 -0500

    Set environment for single pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import {KWaveStash, Home, MainWarehouse, RemoteWarehouse } from './pages'
+import {KWaveStash, Home, MainWarehouse, RemoteWarehouse, Products} from './pages'
 
 export const App = () => {
   return (
@@ -7,11 +7,11 @@ export const App = () => {
           <Routes>
                <Route path="/" element={<Home />} /> {/* home page */}
                 <Route path="/og" element={<KWaveStash />} />
-                <Route  path="/warehouse" element={<MainWarehouse />} /> {/* all products */}
-                <Route /> {/* single product*/}
-                <Route /> {/* all main warehouses*/}
-                <Route /> {/* single warehouse/remote */}
+                <Route  path="/warehouse" element={<MainWarehouse />} /> {/* all warehouses */}
                 <Route path="/remote" element={<RemoteWarehouse />} /> {/*all remote warehouses */}
+                <Route path="/products" element={<Products />} /> {/* all products*/}
+                <Route  /> {/* single product*/}
+                <Route /> {/* single warehouse/remote */}
                 <Route /> {/* add new product */}
                 <Route /> {/* add new warehouse */}
           </Routes>
---
+++ src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import { ProductList } from '../components/Lists/ProductList';
+
+export const Products = () => {
+    return (
+        <div>
+            <h1>All Products</h1>
+            <ProductList />
+        </div>
+        
+    )
+}
